62861a91e940604b36eedfce06740cdb983f79dd
Mike Christof <mhristof@gmail.com>
2021-03-18 17:49:21 +0000

    Setup auth (#1)
    * test
    
    * fix input name
    
    * update name
    
    * update version
    
    * typo
    
    * install curl
    
    * added sleep job
    
    * print out job info
    
    * quote job
    
    * test
    
    * added good job
    
    * wait for jobs to finish
    
    * install jq
    
    * echo runs
    
    * reduce curl output
    
    * wait for other jobs
    
    * added bad job
    
    * show runs after finish line
    
    * exit 1 if a failure is found
    
    * merge if all is good
    
    * disable failing job
    
    * debug messages
    
    * show merge API endpoint
    
    * default value to input variable in bash
    
    * rename dogfood command
    
    * deubg
    
    * fix label filter
    
    * fix merge api url
    
    * add merge method
    
    * sanity check merge method
    
    * show all env vars
    
    * fix merger method name
    
    * debug
    
    * fix label check
    
    * do the deed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,60 @@
 
 set -euo pipefail
 
-echo "current run $GITHUB_RUN_ID"
-curl -i -u "$GITHUB_ACTOR:$GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
+die() { echo "$*" 1>&2 ; exit 1; }
+
+# args:
+# $1: the url to curl
+# $2: method, defaults to GET
+# $3: data, defaults to '{}'
+function api {
+    local url
+    local method
+    local data
+    url=$1
+    method="${2:-GET}"
+    data="${3:-'{}'}"
+
+    curl --header "authorization: Bearer $INPUT_GITHUB_TOKEN" \
+        -X "$method" \
+        -H "Accept: application/vnd.github.v3+json" \
+        --header 'content-type: application/json' \
+        -d "$data" \
+        --silent "$url"
+    }
+
+function runs {
+    api "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
+}
+
+RUNS=/tmp/runs.json
+PR=/tmp/pr.json
+
+while [[ "$(runs | tee $RUNS | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
+    echo "Waiting for jobs to finish"
+    jq '.check_runs[] | select(.status == "in_progress") .name' $RUNS -r
+    sleep 10
+done
+
+if [[ "$(jq '.total_count - 1' $RUNS -r)" -ne "$(jq '[.check_runs[] | select(.conclusion != null and  .conclusion == "success") | .name] | length' /tmp/runs.json -r)" ]]; then
+    echo 'Failure founds, exiting'
+    exit 1
+fi
+
+api "$(jq '.check_runs[0].pull_requests[0].url' $RUNS -r)" > $PR
+
+# shellcheck disable=SC2154
+# shellcheck disable=SC2086
+if [[ "$(jq '[.labels[] | select(.name == "'${INPUT_LABEL:-}'")] | length' $PR)" != "1" ]]; then
+    echo "Label not found - skipping automerge"
+    exit 0
+fi
+
+case "${INPUT_MERGE_METHOD:-}" in
+    merge|squash|rebase)
+        :
+        ;;
+    *) die "Error, unknown merge method $INPUT_MERGE_METHOD";;
+esac
+
+api "$(jq .url $PR -r )/merge" "PUT" "{\"merge_method\": \"${INPUT_MERGE_METHOD:-}\"}"
