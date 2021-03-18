a987dad576dc2f28bbae1466df9530dc71facf08
Mike Christof <mhristof@gmail.com>
2021-03-18 15:47:11 +0000

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 set
 echo "current run $GITHUB_RUN_ID"
-curl --header "authorization: Bearer $GINPUT_GITHUB_TOKEN" \
+curl --header "authorization: Bearer $INPUT_GITHUB_TOKEN" \
         --header 'content-type: application/json' \
         -i \
         "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
