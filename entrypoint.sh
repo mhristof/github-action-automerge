8bb1b962d27238748ac4e53d6ec4a012e5d5dce7
Mike Christof <mhristof@gmail.com>
2021-03-18 16:45:37 +0000

    wait for other jobs

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,7 @@ function runs {
         "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
 }
 
-set
-echo "current run $GITHUB_RUN_ID"
-
-echo "$(runs)"
-#while [[ "$(runs | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
-    #echo "Waiting for jobs to finish"
-    #sleep 10
-#done
+while [[ "$(runs | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
+    echo "Waiting for jobs to finish"
+    sleep 10
+done
