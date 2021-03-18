aba1d052b594ebe9029a65951d38f5f0aca6f531
Mike Christof <mhristof@gmail.com>
2021-03-18 17:10:35 +0000

    merge if all is good

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,36 @@
 
 set -euo pipefail
 
-function runs {
+function api {
+    local url
+    url=$1
+
     curl --header "authorization: Bearer $INPUT_GITHUB_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
         --header 'content-type: application/json' \
-        --silent \
-        "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
+        --silent "$url"
+    }
+
+function runs {
+    api "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
 }
 
-while [[ "$(runs | tee /tmp/runs.json | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
+RUNS=/tmp/runs.json
+
+while [[ "$(runs | tee $RUNS | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
     echo "Waiting for jobs to finish"
-    jq '.check_runs[] | select(.status == "in_progress") .name' /tmp/runs.json -r
+    jq '.check_runs[] | select(.status == "in_progress") .name' $RUNS -r
     sleep 10
 done
 
-runs
+cat $RUNS
 
-if [[ "$(jq '.total_count - 1' /tmp/runs.json -r)" -ne "$(jq '[.check_runs[] | select(.conclusion != null and  .conclusion == "success") | .name] | length' /tmp/runs.json -r)" ]]; then
+if [[ "$(jq '.total_count - 1' $RUNS -r)" -ne "$(jq '[.check_runs[] | select(.conclusion != null and  .conclusion == "success") | .name] | length' /tmp/runs.json -r)" ]]; then
+    echo 'Failure founds, exiting'
     exit 1
+fi
+
+
+if [[ "$(api "$(jq '.check_runs[0].pull_requests[0].url' $RUNS -r)" | jq '.labels | index( "automerge" )')" != "null" ]]; then
+    echo "merging PR"
 fi
