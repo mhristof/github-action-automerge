c20dba263e8e2a36d0b4ae53df8092b4c0caec9e
Mike Christof <mhristof@gmail.com>
2021-03-18 16:41:33 +0000

    wait for jobs to finish

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,18 @@
 
 set -euo pipefail
 
-set
-echo "current run $GITHUB_RUN_ID"
-curl --header "authorization: Bearer $INPUT_GITHUB_TOKEN" \
+function runs {
+    curl --header "authorization: Bearer $INPUT_GITHUB_TOKEN" \
         --header 'content-type: application/json' \
+        --silent \
         -i \
         "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/check-runs"
+}
+
+set
+echo "current run $GITHUB_RUN_ID"
+
+while [[ "$(runs | jq '[.check_runs[] | select(.status == "in_progress") .status] | length')" -ne 1 ]]; do
+    echo "Waiting for jobs to finish"
+    sleep 10
+done
